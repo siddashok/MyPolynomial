82ab1ee3e22e99512361e37ab37ec0e3810eb00f
Patro, Siddharth (INFOSYS) <siddharth.patro1@bp.com>
2020-06-18 18:49:50 -0500

    First commit

---
+++ MyPolynomial/MyPolynomial.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPolynomial
+{
+    class MyPolynomial
+    {
+        private readonly  double[] _coeffs;
+
+        public MyPolynomial(double[] coeffs)
+        {
+            _coeffs = new double[coeffs.Length];
+
+            for (int i = 0; i < coeffs.Length; i++)
+            {
+                _coeffs[i] = coeffs[i];
+            }
+        }
+
+        int GetDegree()
+        {
+            return _coeffs.Length - 1;
+        }
+
+        public override String ToString()
+        {
+            String str = "";
+            for (int i = _coeffs.Length - 1; i >= 0; i--) //5-1 = 4 i=4, i>=0;i-- 4,3,2,1,0
+            {
+                if (_coeffs[i] > 0)
+                    str = str + "+" + _coeffs[i] + "x^" + i;
+                else if (_coeffs[i] < 0)
+                    str = str + _coeffs[i] + "x^" + i;
+            }
+            return str;
+        }
+
+        public  double Evaluate(double x)
+        {
+            double EvaluatedValue = 0;
+            for (int i = 0; i < _coeffs.Length; i++)
+            {
+                EvaluatedValue += _coeffs[i] * Math.Pow(x, i);
+            }
+            return EvaluatedValue;
+        }
+
+        public MyPolynomial Add(MyPolynomial another)
+        {
+            int a = GetDegree();
+            int b = another.GetDegree();
+            int value;
+            if (a <= b)
+                value = b + 1;
+            else
+                value = a + 1;
+
+            double[] NewPolynomial = new double[value];
+
+            int i;
+            for (i = 0; i < a + 1 && i < b + 1; i++)
+            {
+                NewPolynomial[i] = _coeffs[i] + another._coeffs[i];
+            }
+
+            for (; i < a + 1; i++)
+            {
+                NewPolynomial[i] = _coeffs[i];
+            }
+
+            for (; i < b + 1; i++)
+            {
+                NewPolynomial[i] = another._coeffs[i];
+            }
+
+            MyPolynomial SumPolynomial = new MyPolynomial(NewPolynomial);
+
+            return SumPolynomial;
+        }
+
+        public MyPolynomial Multiply(MyPolynomial another)
+        {
+            int a = GetDegree();
+            int b = another.GetDegree();
+            int value = ((a + 1) * (b + 1)) + 1;
+
+            double[] NewPolynomial = new double[value];
+
+            for (int i = 0; i < a + 1; i++)
+            {
+                for (int j = 0; j < b + 1; j++)
+                {
+                    NewPolynomial[i + j] += _coeffs[i] * another._coeffs[j];
+                }
+            }
+
+            MyPolynomial MultiplyPolynomial = new MyPolynomial(NewPolynomial);
+
+            return MultiplyPolynomial;
+        }
+    }
+}
---
+++ MyPolynomial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyPolynomial
+{  
+      public class program
+    { 
+         public static void Main(string[] args)
+        {
+            double[] polynomial1 = { 2, 1, -3, 0, 3 };       //3x^4 - 3x^2 + 1x^1 + 2x^0
+            double[] polynomial2 = { 4, -1, 1, -4,  2 };    //2x^4 - 4x^3 + 1x^2 - 1x^1 + 4x^0
+            MyPolynomial p1 = new MyPolynomial(polynomial1);
+            MyPolynomial p2 = new MyPolynomial(polynomial2);
+            
+            Console.WriteLine("First Polynomial: ");
+            Console.WriteLine(p1 + "\n");
+            Console.WriteLine("Second Polynomial: ");
+            Console.WriteLine(p2 + "\n");
+
+            Console.WriteLine("Provide the Value of X for Evaluation");
+            int x_val = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Evaluated Value of Polynomial 1:");
+            Console.WriteLine(p1.Evaluate(x_val));
+            Console.WriteLine("Evaluated Value of Polynomial 2:");
+            Console.WriteLine(p2.Evaluate(x_val));
+
+            Console.WriteLine("Polynomial Addition: ");
+            MyPolynomial polynomial_add = p1.Add(p2);
+            Console.WriteLine(polynomial_add + "\n");
+            Console.WriteLine("Polynomial Multiplication: ");
+            MyPolynomial polynomial_mul = p1.Multiply(p2);
+            Console.WriteLine(polynomial_mul + "\n");
+        }
+    }
+}
